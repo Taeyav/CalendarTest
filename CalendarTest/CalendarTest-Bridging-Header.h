6fda3282c0db424ed29ae76ec2c9af43a9e8b793
Taeya <Taeya@taeyadexiaokeai.local>
2019-01-14 20:24:59 +0800

    在ViewController中通过代码实现日历的呈现和样式修改

---
+++ CalendarTest/CalendarTest-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#ifndef CalendarTest_Bridge_Header_h
+#define CalendarTest_Bridge_Header_h
+
+#import "FSCalendar.h"
+
+#endif
--- CalendarTest/ViewController.swift
+++ CalendarTest/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate{
+    
+    private weak var calendar: FSCalendar!
+    override func loadView() {
+        
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = UIColor.groupTableViewBackground
+        self.view = view
+        
+        let height: CGFloat = UIDevice.current.model.hasPrefix("iPad") ? 400 : 300
+        let calendar = FSCalendar(frame: CGRect(x: 0, y: self.navigationController!.navigationBar.frame.maxY, width: self.view.bounds.width, height: height))
+        calendar.dataSource = self
+        calendar.delegate = self
+        calendar.backgroundColor = UIColor.white //设置日历背景色
+        calendar.appearance.headerTitleColor = UIColor.black //设置header标题字体颜色
+        calendar.appearance.headerMinimumDissolvedAlpha = 0;//调整header前后月份标签静止时刻透明
+        calendar.appearance.headerDateFormat = "yyyy年MM月"; //调整日历hear的年月格式
+        calendar.appearance.weekdayTextColor = UIColor.black //设置星期抬头字体颜色
+        calendar.appearance.caseOptions = FSCalendarCaseOptions(rawValue: 1 << 4);//通过FSCalendarAppearance.h找到需要的格式对应的值数 设置星期抬头格式
+        calendar.allowsMultipleSelection = false; //不允许多选日期
+        calendar.appearance.weekdayTextSize = 15;
+        
+        self.view.addSubview(calendar)
+        self.calendar = calendar
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "日记"
     }
-
-
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        if monthPosition == .previous || monthPosition == .next {
+            calendar.setCurrentPage(date, animated: true)
+        }
+    }
+    
 }
 
